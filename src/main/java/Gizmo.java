e034c71bc1bb626f57cb67f0125909218880bcfa
jamestruuong <jatruong2@gmail.com>
2025-04-23 11:27:08 -0700

    Update Gizmo.java

--- src/main/java/Gizmo.java
+++ src/main/java/Gizmo.java
@@ -30,21 +30,4 @@ public class Gizmo
     maker = s;
     isElec = e;
   }
-  public int countElectronicsByMaker(string maker){
-    int num = 0; 
-    for(Gizmo g: purchases){
-      if(g.isElectronic() && g.getMaker().equals(maker)){
-        num++;
-      }
-    }
-    return num;
-  }
-
-  public boolean hasAdjacentEqualPair(){
-    for(int i = 0; i<purchases.size()-1; i++){
-      if(purchases.get(i).equals(purchases.get(i+1))){
-        return true;
-      }
-    }return false;
-  }
 }
